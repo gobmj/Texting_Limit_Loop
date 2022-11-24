f7de797658ed9568e65508bd54b4b9ceb1357d07
Govind M J <govindmj2002@gmail.com>
2022-11-25 02:23:02 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Code to loop a text, given the limit
+
+import pyautogui as pt
+import time
+
+limit = input("Enter Limit to Loop: ")
+message = input("Enter message to Loop: ")
+time.sleep(5)
+
+i = 0
+
+while i < int(limit):
+    pt.typewrite(message)    
+    pt.press("enter")
+    i+=1
